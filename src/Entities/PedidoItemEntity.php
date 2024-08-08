ebc4a50ec21d5aa711e00a3c11869c1159f4d894
Anderson Andrade <anderson.asa89@gmail.com>
2024-08-07 21:48:57 -0300

    refactor

--- src/Entities/PedidoItemEntity.php
+++ src/Entities/PedidoItemEntity.php
@@ -5,6 +5,7 @@ namespace Prettus\TinyERP\Entities;
 
 class PedidoItemEntity extends AbstractEntity
 {
+    public readonly ?int $id_produto;
     public readonly ?string $codigo;
     public readonly ?string $descricao;
     public readonly ?string $unidade;
