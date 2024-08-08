e021156a0198d53d464d1a0def1d52416aea5cff
Anderson Andrade <anderson.asa89@gmail.com>
2024-08-07 21:55:20 -0300

    refactor

--- src/Entities/PedidoClienteEntity.php
+++ src/Entities/PedidoClienteEntity.php
@@ -7,4 +7,26 @@ class PedidoClienteEntity extends AbstractEntity
 {
     public readonly ?string $codigo;
     public readonly ?string $nome;
+    public readonly ?string $tipo_pessoa;
+    public readonly ?string $fantasia;
+    public readonly ?string $cpf_cnpj;
+    public readonly ?string $ie;
+    public readonly ?string $rg;
+    public readonly ?string $endereco;
+    public readonly ?string $numero;
+    public readonly ?string $complemento;
+    public readonly ?string $bairro;
+    public readonly ?string $cep;
+    public readonly ?string $cidade;
+    public readonly ?string $uf;
+    public readonly ?string $pais;
+    public readonly ?string $fone;
+    public readonly ?string $email;
+
+    public static function sourceMapping(): array
+    {
+        return [
+            'nome_fantasia'=>'fantasia',
+        ];
+    }
 }
