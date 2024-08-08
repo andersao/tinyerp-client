ba3f15dae08c1ce5630402f1cbd4ee1db43f570c
Anderson Andrade <anderson.asa89@gmail.com>
2024-08-07 21:52:02 -0300

    refactor

--- src/Entities/PedidoParcelaEntity.php
+++ src/Entities/PedidoParcelaEntity.php
@@ -9,4 +9,6 @@ class PedidoParcelaEntity extends AbstractEntity
     public readonly ?string $data;
     public readonly ?float $valor;
     public readonly ?string $obs;
+    public readonly ?string $forma_pagamento;
+    public readonly ?string $meio_pagamento;
 }
